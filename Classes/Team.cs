b94c7e209d97cd4a3c6bf582cd008d7db46eefad
Austin <langleyaustin0214@gmail.com>
2022-06-17 14:59:34 -0500

    fdfs

---
+++ Classes/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThePlayer.Classes
+{
+    internal class Team
+    {
+        public string QB { get; set; }
+        public string WR { get; set; }
+        public string RB { get; set; }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return QB;
+                else if (index == 1)
+                    return WR;
+                else
+                    return RB;
+            }
+            set
+            {
+                if (index == 0)
+                    QB = value;
+                else if (index == 1)
+                    WR = value;
+                else
+                    RB = value;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.Json;
+using System.IO;
+using GuessThePlayer.Classes;
+
+namespace GuessThePlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static int index = 0;
+        private Team teamObject;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string filename = "C:/Users/alangley/Desktop/Stuff/Football/GuessThePlayer/GuessThePlayer/Team.json";
+            string teamJson = File.ReadAllText(filename);
+            teamObject = JsonSerializer.Deserialize<Team>(teamJson);
+        }
+
+        private void New_Player_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //var teams = new List<Team>
+            //{
+            //    new Team {QB = "Test QB", WR = "Test WR", RB = "Test RB"},
+            //    new Team {QB = "Test QB1", WR = "Test WR1", RB = "Test RB1"}
+            //};
+
+            lblNewPlayer.Content = teamObject[index];
+            index = (index < 2) ? index + 1 : 0;
+            //lblNewPlayer.Content = (JsonSerializer.Serialize(teams));
+        }
+
+        
+    }
+}
